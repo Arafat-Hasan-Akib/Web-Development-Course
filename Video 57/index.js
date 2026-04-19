d113adb997e35a6ea29cb9bd4da27c6bfdb90d0b
Arafat Hasan Akib <websited98@gmail.com>
2026-04-19 21:55:59 +0600

    Add files via upload

---
+++ Video 57/index.js
@@ -0,0 +1,43 @@
+console.log("I am a tutorial of Loops")
+
+let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+// console.log(a+2)
+
+
+// for (let i = 0; i < 100; i++) {
+//     console.log(a + i);
+// }
+
+// let obj = {
+//     name: "Akib",
+//     role: "Programmer",
+//     company: "CodeWithAkib AI"
+
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+// }
+
+// for (const c of "Akib") {
+//     console.log(c)
+// }
+
+let i = 0;
+while (i<6) {
+    console.log(i)
+    i++;
+
+}
+
+
+// let i = 0;
+// do {
+//     console.log(i)
+//     i++;
+
+// } while (condition);
