e1d69fdae80c33c7da164217dd8a98ddb9300766
Arafat Hasan Akib <websited98@gmail.com>
2026-04-19 21:53:50 +0600

    Add files via upload

---
+++ Video 55/script.js
@@ -0,0 +1,45 @@
+console.log("Hey this is tutorial 55 of Js")
+
+let a = 5;
+let b = 6;
+let c = "Akib";
+let _a = "Akib";
+// var 55a = "Akib"; not allowed
+
+console.log(a + b + 8)
+console.log(typeof a, typeof b, typeof c)
+{
+    console.log(a)
+}
+
+// const a1 = 6;
+// a1 = a1 + 1;
+// not allowed because a1 is constant
+
+
+
+let x = "Akib";
+let y = 22;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+
+
+let o = {
+    "name" : "Akib",
+    "job role": 5600
+
+}
+
+console.log(o);
+o.salary = "100crores";
+console.log(o);
+o.salary = "500crores";
+console.log(o)
+
+
