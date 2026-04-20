1b6ca2befcaf5e8ee10296ffabb62f4319532851
Arafat Hasan Akib <websited98@gmail.com>
2026-04-20 20:25:42 +0600

    Add files via upload

---
+++ Video 67/script.js
@@ -0,0 +1,5 @@
+console.log("Hello world")
+
+document.body.firstElementChild  //prothom emon child jeita element
+document.body.firstElementChild.childNodes //eita dile shob nodes ashbo jeitate text node, element etc thakbo
+document.body.firstElementChild.children //eita dile shudhu eitar bhitorer html element paoya jay
